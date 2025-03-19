86289d95f38e36149d9820cec5a23d7324b5cf68
Elizabeth Pauley <elizpauley@gmail.com>
2025-03-19 14:23:56 -0700

    Adding more initial updates

--- Embrace/obj/Debug/net8.0/Embrace.AssemblyInfo.cs
+++ Embrace/obj/Debug/net8.0/Embrace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ba30d0f896feffde17e60b09b003eae02ff8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
