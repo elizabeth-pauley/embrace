4007d23924830d83864baa7f68e30a84fcb24d20
Elizabeth Pauley <elizpauley@gmail.com>
2025-04-13 10:31:01 -0600

    more document translation updates

--- Embrace/obj/Debug/net8.0/Embrace.AssemblyInfo.cs
+++ Embrace/obj/Debug/net8.0/Embrace.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129963ad1c63ed0d282e78211db4d74f8f0cd314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd82d63f35b33164fabc5f522262edd9759cb1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Embrace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
